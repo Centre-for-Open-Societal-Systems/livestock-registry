4a6b4b4917edec53289c9ea9421232e3115e7b02
N5IZ <www.nahom.nasiz@gmail.com>
2026-08-18 00:41:52 +0300

    latest livestock with dashboard

--- livestock-extension/src/openg2p_registry_livestock_extension/meta_data/register-metadata/g2p_intake_form_ui_tab_sections.sql
+++ livestock-extension/src/openg2p_registry_livestock_extension/meta_data/register-metadata/g2p_intake_form_ui_tab_sections.sql
@@ -7,7 +7,6 @@
 -- where they are read.
 INSERT INTO "public"."g2p_intake_form_ui_tab_sections" ("tab_section_id","tab_id","section_id","section_order") VALUES
 ('intake_tab_section_1','0ebdc221-187d-5df6-9dc3-c6f4c4ee160e','livestock_farmer_identity_section_01',10),
-('intake_tab_section_3','0ebdc221-187d-5df6-9dc3-c6f4c4ee160e','livestock_livestock_record_section_01',30),
 ('intake_tab_section_4','0ebdc221-187d-5df6-9dc3-c6f4c4ee160e','livestock_survey_personnel_section_02',40),
 ('intake_tab_section_5','0ebdc221-187d-5df6-9dc3-c6f4c4ee160e','livestock_livestock_location_section_03',50),
 ('intake_tab_section_6','0ebdc221-187d-5df6-9dc3-c6f4c4ee160e','livestock_animal_details_section_01',60),
